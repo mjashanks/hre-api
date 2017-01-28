6e831e5b165eebb2458d434c565880fc84a76fa3
Mike <michael.shanks@blue-zinc.com>
2017-01-28 22:37:23 +0000

    serving static files

---
+++ Controllers/StaticFilesController.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Hre.Api.Database;
+using System.IO;
+using Hre.Api.Extensions;
+using System.Reflection;
+using System.Text;
+
+namespace Hre.Api.Controllers
+{
+    [Route("")]
+    public class StaticFilesController : Controller
+    {
+
+        private byte[] GetResource(string key)
+        {
+            var assembly = GetType().GetTypeInfo().Assembly;
+            var resourceStream = assembly.GetManifestResourceStream("hre-api.StaticFiles." + key);
+            using (var reader = new BinaryReader(resourceStream))
+            {
+                return reader.ReadBytes((int) resourceStream.Length);
+            }            
+        }
+
+        [HttpGet]
+        public IActionResult GetIndex(string file)
+        {
+            return File(GetResource("index.html"), "text/html");
+        }
+
+        [HttpGet("{file}")]
+        public IActionResult GetRootFile(string file)
+        {
+            return File(GetResource(file), file.AsContentType());
+        }
+
+        [HttpGet("scripts/{file}")]
+        public IActionResult GetScript(string file)
+        {
+            return File(GetResource("Scripts."+ file), file.AsContentType());
+        }
+    }
+}
--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Hre.Api.Extensions
+﻿using System.IO;
+
+namespace Hre.Api.Extensions
 {
     public static class StringExtensions
     {
@@ -12,18 +14,25 @@
             return str + " where " + clause;
         }
 
-        public static string AsContentType(this string ext)
+        public static string AsContentType(this string file)
         {
+            var ext = Path.GetExtension(file);
             switch (ext)
             {
-                case "jpg":
+                case ".jpg":
                     return "image/jpeg";
-                case "gif":
+                case ".gif":
                     return "image/gif";
-                case "png":
+                case ".png":
                     return "image/png";
-                case "bmp":
+                case ".bmp":
                     return "image/bmp";
+                case ".html":
+                    return "text/html";
+                case ".js":
+                    return "text/javascript";
+                case ".ico":
+                    return "image/x-icon";
                 default:
                     return "image/jpg";
             }
